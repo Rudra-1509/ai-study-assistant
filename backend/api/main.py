f930feea7ae37564049e7ee595bde4d0a19ac867
Rudra-1509 <rudranilmn1509@gmail.com>
2026-01-31 19:08:31 +0530

    fixed localhost to ngork

--- backend/api/main.py
+++ backend/api/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://noncontingent-hypodermically-shawana.ngrok-free.dev",
-        "https://*.vercel.app",
+        "https://ai-study-assistant-sand.vercel.app/",
     ],
     allow_origins=["*"],
     allow_credentials=True,
